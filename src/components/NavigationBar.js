8752f67797b38a92955abe073701153fe7bd00af
Manikanta Nallagatla <mnallagatla@microsoft.com>
2022-08-03 12:50:09 +0530

    Fix bug of 404

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 import PropTypes from 'prop-types'
+import ToDoList from './TodoList'
 import NavigationStyle from '../style/navigation.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Login from './login';
@@ -26,12 +27,22 @@ function app() {
     );
 }
 
+function dashboard() {
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(
+    <React.StrictMode>
+        <NavigationBar/>
+        <ToDoList/>
+    </React.StrictMode>
+    );
+}
+
 function NavigationBar() {
     return (
         <div className="blackBackground">
             <ul>
                 <a href="/"><li><i className="fa fa-fw fa-home" />  <span>Admin Center</span></li></a>
-                <a href="/driDashboard"><li><i className="fa fa-fw fa-home" />  <span>DRI Dashboard</span></li></a>
+                <a onClick={dashboard}><li><i className="fa fa-fw fa-home" />  <span>DRI Dashboard</span></li></a>
                 <div onClick={logout} className="floatRight">Logout</div>
             </ul>
         </div>
